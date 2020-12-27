be3b8f241fbe387871ec06b433b3c5becdb9f107
Graham Breed <gbreed@gmail.com>
2020-12-27 12:16:58 +0000

    define some constants as const

--- parametric.jl
+++ parametric.jl
@@ -1,9 +1,9 @@
 push!(LOAD_PATH, ".")
 using TE
 
-marvel = [22 35 51 62 76; 31 49 72 87 107; 41 65 95 115 142]
-magic = [22 35 51 62 76; 41 65 95 115 142]
-limit11 = limit(11)
+const marvel = [22 35 51 62 76; 31 49 72 87 107; 41 65 95 115 142]
+const magic = [22 35 51 62 76; 41 65 95 115 142]
+const limit11 = limit(11)
 
 println("Magic complexity ", rms_of_matrix(magic ./ limit11))
 println("Marvel complexity ", rms_of_matrix(marvel ./ limit11))
