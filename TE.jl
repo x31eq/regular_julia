1cf17d88dbb77fe95ae3e57efe9cb4a2e0d9471a
Graham Breed <gbreed@gmail.com>
2020-12-27 13:30:33 +0000

    Single character for <=

--- TE.jl
+++ TE.jl
@@ -6,7 +6,7 @@ export limit, rms_of_matrix, te_complexity, optimal_badness, cangwu
 
 const primes = [2 3 5 7 11 13 17 19 23 29 31]
 
-limit(n) = log2.(transpose(primes[primes .<= n]))
+limit(n) = log2.(transpose(primes[primes .≤ n]))
 
 rms_of_matrix(W) = prod(svdvals(W / √size(W, 2)))
 
